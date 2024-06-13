fccdd4234dbaf405a49e908044ac16254e203fea
Ibrahim <mohamedibrahima2023@gmail.com>
2024-06-12 19:13:17 -0500

    First Java

---
+++ Session32f.java
@@ -0,0 +1,12 @@
+public class Session32f {
+
+    public static void main(String args[]) {
+        int[] x = {10, 20, 30, 40};
+        for (int i = x.length - 1; i >= 0; i--) {
+
+            System.out.println(x[i]);
+        }
+
+
+    }
+}
